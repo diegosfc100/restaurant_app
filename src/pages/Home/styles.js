e5810578372282255e4a31867017143135f6d909
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-23 12:48:34 -0300

    Style carousel

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export  const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+
+
+`
+
 export const Container = styled.aside`
   background-color: ${(props) => props.theme.colors.background};
   width: 360px;
@@ -19,4 +26,18 @@ export const Search = styled.section`
 
 export const Logo = styled.img`
     margin-bottom: 15px;
+`;
+
+export const Map = styled.div`
+    background-color: red;
+    width: 500px;
+`
+
+export const CarouselTitle = styled.h1`
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: ${(props) => props.theme.colors.text};
+    font-size: 24px;
+    font-weight: bold;
+    line-weigth: 29px;   
+    margin: 16px;
 `;
