566e466a5e1b124f01db42c64f64d192ce2b8ee0
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-23 11:26:26 -0300

    Style container, search field and logo

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
- // background-color: ${(props) =>
-   props.theme.colors.primary}; /*primary é a cor definida no theme.js*/
+export const Container = styled.aside`
+  background-color: ${(props) => props.theme.colors.background};
+  width: 360px;
+  height: 100vh; 
+  overflow-y: auto; 
+`;
+/*A medida vh é igual a 1/100 da altura da viewport. Então, por exemplo, se a altura do navegador é 900px, 1vh equivale a 9px e, analogamente, se a largura da viewport é 750px, 1vw equivale a 7.5px.*/
+/*O conteúdo é acessível através de barras de rolagem que são exibidas mesmo que o conteúdo não precise. Isso evita o problema de barras de rolagem aparecendo e desaparecendo quando o conteúdo é dinâmico. */
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 16px;
+`;
+
+export const Logo = styled.img`
+    margin-bottom: 15px;
 `;
