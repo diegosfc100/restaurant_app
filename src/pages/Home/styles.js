d448adce372d62988e21aee6f5b0724ae3561e38
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-23 15:42:02 -0300

    Styled carousel

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
+import Slider from 'react-slick';
+
 export  const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
+`;
 
-
-`
+/* Ao estilizar uma tag ao algo nativo, basta utilizar o ponto. >>> ex: styled.section
+   Para estilizar um componente, usa-se parenteses. >>> ex: styled(Slider) */
 
 export const Container = styled.aside`
   background-color: ${(props) => props.theme.colors.background};
@@ -13,6 +16,7 @@ export const Container = styled.aside`
   height: 100vh; 
   overflow-y: auto; 
 `;
+
 /*A medida vh é igual a 1/100 da altura da viewport. Então, por exemplo, se a altura do navegador é 900px, 1vh equivale a 9px e, analogamente, se a largura da viewport é 750px, 1vw equivale a 7.5px.*/
 /*O conteúdo é acessível através de barras de rolagem que são exibidas mesmo que o conteúdo não precise. Isso evita o problema de barras de rolagem aparecendo e desaparecendo quando o conteúdo é dinâmico. */
 
@@ -32,6 +36,11 @@ export const Map = styled.div`
     background-color: red;
     width: 500px;
 `
+export const Carousel = styled(Slider)`
+    .slick-slide {
+        margin-right: 16px;
+    }
+`;
 
 export const CarouselTitle = styled.h1`
     font-family: ${(props) => props.theme.fonts.regular};
