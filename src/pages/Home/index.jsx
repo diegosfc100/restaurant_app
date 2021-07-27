2ec8a5363ba67df8eddabc1fe034b05a244bcc6b
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-27 20:38:03 -0300

    Set query

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,6 +11,7 @@ import { Container, Carousel, Search, Logo, Wrapper, CarouselTitle } from './sty
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
+  const [query, setQuery] = useState(null);
   const [modalOpened, setModalOpened] = useState(true);
 
   const settings = {
@@ -21,6 +22,12 @@ const Home = () => {
     slidesToScroll: 4,
     adaptiveHeight: true,
   };
+
+  function handleKeyPress(e) {
+    if (e.key === 'Enter') {
+        setQuery(inputValue);
+    }
+  }
 
     return (
         <Wrapper>
@@ -33,7 +40,11 @@ const Home = () => {
                     trailingIcon={<MaterialIcon role="button" icon="search" />}
 
                     >
-                    <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                    <Input
+                     value={inputValue}
+                     onKeyPress={handleKeyPress} 
+                     onChange={(e) => setInputValue(e.target.value)}
+                     />
                     </ TextField>
                     <CarouselTitle> Na sua Área</CarouselTitle>
                     <Carousel { ...settings}>
@@ -49,7 +60,7 @@ const Home = () => {
                 </ Search>
                 <RestaurantCard />                
             </ Container>
-            <Map />
+            <Map query={query}/>
             <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />
          </ Wrapper>
     );
